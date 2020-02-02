079bfb6dedc921aef73f8a71cc5439d1d7b524ac
Esme01 <TXQ01020304@163.com>
2020-02-02 20:25:43 +0800

    1

--- build/test.cpp
+++ build/test.cpp
@@ -1,8 +1,26 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
+#include<math.h>
 using namespace std;
 
 int main()
 {
-	int a=1,b=2,c=3,d=4;
-	int e=a+b;
+	for(double i;;i++)
+	{
+		if(i+1<i)
+		{
+			printf("%.15lf",i);
+			break;
+		}
+	}
+
+	for(double i;;i--)
+	{
+		if(i-1>i)
+		{
+			printf("%.15lf",i);
+			break;
+		}
+	}
+	return 0;
 }
