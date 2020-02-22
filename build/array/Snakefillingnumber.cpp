1139d2121c02fedd456ab79ad68e46ee2d4f7d72
Esme01 <TXQ01020304@163.com>
2020-02-22 17:24:13 +0800

    snake

--- build/array/Snakefillingnumber.cpp
+++ build/array/Snakefillingnumber.cpp
@@ -3,13 +3,13 @@
 #include<cmath>
 #include<stdlib.h>
 #include<string.h>
-#define MAXN 1000+10
+#define MAXN 10
 int a[MAXN][MAXN];
 //感觉是用到二维数组
 using namespace std;
 
 int main(){
-    memset(a,0,sizeof(int));
+    memset(a,0,sizeof(a));
     int n,x,y,total=0;
     scanf("%d",&n);
     total=a[x=0][y=n-1]=1;
@@ -18,7 +18,7 @@ int main(){
         while (x+1<n&&!a[x+1][y])a[++x][y]=++total;
         while (y-1>=0&&!a[x][y-1])a[x][--y]=++total;
         while (x-1>=0&&!a[x-1][y])a[--x][y]=++total;
-        while (y+1<n&&!a[x][++y])a[x][++y]=++total;
+        while (y+1<n&&!a[x][y+1])a[x][++y]=++total;
     }
     for (x = 0; x < n; x++)
     {
