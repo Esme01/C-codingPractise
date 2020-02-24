6c5bbb12d7e5e834e0169fb512365280ac715bb7
Esme01 <TXQ01020304@163.com>
2020-02-24 15:19:01 +0800

    回文

---
+++ build/array/LongestPalindromicSubstring.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<ctype.h>
+#define MAXN 5000+10
+char s[MAXN],buf[MAXN];
+using namespace std;
+
+int main()
+{
+    //之前做的题都是判断一个字符串是否是回文
+    //这道题却是在一个字符串中找到最长的回文
+    //scanf("%s",s);
+    int n,m=0,max=0;
+    int i,j,k;
+    fgets(buf,sizeof(s),stdin);//stdin标准流输入
+    n=strlen(buf);
+    for ( i = 0; i < n; i++)
+        if (isalpha(buf[i]))s[m++]=toupper(buf[i]);
+        //m为字符串长度
+    //字符串预处理
+    for ( i = 0; i < m; i++)
+    {
+        /* code */
+        for ( j = i; j < m; i++)
+        {
+            /* code */
+            int ok=1;
+            for ( k = i; k <=j; k++)
+                if (s[k]!=s[j+i-k])ok=0;
+            if (ok&&j-i+1>max)max=j-i+1;
+        }
+    }
+    printf("max=%d\n",max);
+    return 0; 
+}
--- build/test.cpp
+++ build/test.cpp
@@ -10,22 +10,40 @@ int main()
     int i,ok,abc,de,x,y,z,count=0;
     char s[20],buf[99];
     scanf("%s",s);
-    for ( abc = 111; abc < 999; abc++)
+    for ( abc = 111; abc <=999; abc++)
     {
         /* code */
-        for ( de = 0; de < count; de++)
+        for ( de = 0; de <= 99; de++)
         {
             /* code */
-            x=abc*(x%10);
-            y=abc*(x/10);
+            x=abc*(de%10);
+            y=abc*(de/10);
             z=abc*de;
-            sprintf(buf,x,y,z);
+            sprintf(buf,"%d%d%d%d%d",x,y,z,abc,de);
             //sprintf的书写格式？
-
+            ok=1;
+            for ( i = 0; i < strlen(buf); i++)
+            {
+                /* code */
+                if (strchr(s,buf[i])==NULL)
+                {
+                    /* code */
+                    ok=0;
+                    break;
+                }
+            }
+            if (ok)
+            {
+                //count++;
+                /* code */
+                printf("<%d>\n",++count);
+                printf("%5d\nX%4d\n----\n%5d\n%4d\n----\n%5d\n",abc,de,x,y,z);
+                
+            }
         }
         
     }
-    
+    printf("The number of solutions=%d",count);
    
    return 0; 
 }
