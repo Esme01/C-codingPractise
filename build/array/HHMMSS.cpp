035fde82b10576de5692434b69e317484b2150c3
Esme01 <TXQ01020304@163.com>
2020-02-25 11:48:43 +0800

    sscanf

---
+++ build/array/HHMMSS.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAXN 100
+using namespace std;
+/**
+ * 学会sscanf正则表达式的用法
+*/
+char s[MAXN];
+int main()
+{
+    char HH[2],MM[2],SS[2];
+    int hh,mm,ss;
+    fgets(s,sizeof(s),stdin);
+    sscanf(s,"%[^:]:%[^:]:%2s",HH,MM,SS);
+    hh=atoi(HH);
+    mm=atoi(MM);
+    ss=atoi(SS);
+    printf("%d %d %d\n",hh,mm,ss);
+    return 0;
+}
