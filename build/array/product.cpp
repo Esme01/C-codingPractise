3623d6b5cf9e08ad4c6237d6f110626496cf99db
Esme01 <TXQ01020304@163.com>
2020-02-26 19:37:36 +0800

    product

--- build/array/product.cpp
+++ build/array/product.cpp
@@ -1,1 +1,26 @@
 #include<stdio.h>
+#include<iostream>
+#include<ctype.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    char t;
+    int num;
+    scanf("%c",&t);
+    if (isdigit(t))
+    //不能直接判断整数
+    {
+        num=atoi(t)%1000;
+    }
+    
+    // else
+    // {
+    //     cout<<"0"<<endl;
+    // }
+    
+    
+    cout<<t<<endl;
+    return 0;
+}
