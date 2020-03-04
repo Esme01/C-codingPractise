d97c8447c358693921432e57fa49af02371ef73d
Esme01 <TXQ01020304@163.com>
2020-03-04 16:47:07 +0800

    1

--- build/test.cpp
+++ build/test.cpp
@@ -1,33 +1,32 @@
-//网上代码借鉴：
-#include<stdio.h>
-#include<iostream>
+# include<stdio.h>
+# include<ctype.h>
+# include<iostream>
+# include<string.h>
+# define MAXN 1000+10
 
 using namespace std;
-int A[3000]={1};         
-//初始化为1 而不是0 
-//A[0]表示个位
+
+char s[MAXN][MAXN],ss[MAXN][MAXN];
 int main()
 {
-    int n,i,j,s=0;
-    scanf("%d",&n);
-    for(i=2;i<=n;i++)
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
     {
-        for(j=0;j<3000;j++) 
- //因为不知道这个大数会有多少位，因此可以把这3000位都遍历一遍
+        for (int j = 0; j < n; j++)
         {
-            int m=A[j]*i+s;   //同样是多项式乘法，用m作辅助
-            A[j]=m%10;    
-            s=m/10;           //求需要进位的数 
+            cin>>s[n-j-1][i];
         }
     }
 
-    i=2999;     //为逆向输出做准备
-    while(!A[i--]);     //从第一个非零的项开始输出
-    cout<<A[i+100]<<endl;
-    for(j=i+1;j>=0;j--)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d",A[j]);
+        for (int j = 0; j < n; j++)
+        {
+            cout<<s[i][j]<<" ";
+        }
+        cout<<endl;
     }
-    printf("\n");
     return 0;
+    
 }
