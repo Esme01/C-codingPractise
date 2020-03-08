09f3398c850973f5a68742c8dca5b001f6b7357d
Esme01 <TXQ01020304@163.com>
2020-03-08 10:59:30 +0800

    TeX

---
+++ build/Ⅴalgorithm/TeX.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#define MAXN 1000+10
+
+char s[MAXN];
+char ss[MAXN];
+int main()
+{
+    scanf("%s",s);
+    int i=0;
+    while (s[i]||s[i]!='"')i++;
+    if(s[i]=='\n')return 0;
+    memcpy(ss,s,sizeof(char)*i);
+    ss[i]='`';
+    ss[i+1]='`';
+    int j=i+1;
+    for (; s[i]; i++)ss[++j]=s[i];
+    for (int t = 0; ss[t]; t++)printf("%c ",ss[t]);
+    return 0;
+}
