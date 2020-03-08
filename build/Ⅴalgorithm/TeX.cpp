9d2d3ebb11b32daf4a7cecafac170d73149d9572
Esme01 <TXQ01020304@163.com>
2020-03-08 11:04:13 +0800

    TeX

--- build/Ⅴalgorithm/TeX.cpp
+++ build/Ⅴalgorithm/TeX.cpp
@@ -1,20 +1,36 @@
-#include<stdio.h>
-#include<string.h>
-#define MAXN 1000+10
+// #include<stdio.h>
+// #include<string.h>
+// #define MAXN 1000+10
 
-char s[MAXN];
-char ss[MAXN];
+// char s[MAXN];
+// char ss[MAXN];
+// int main()
+// {
+//     scanf("%s",s);
+//     int i=0;
+//     while (s[i]||s[i]!='"')i++;
+//     if(s[i]=='\n')return 0;
+//     memcpy(ss,s,sizeof(char)*i);
+//     ss[i]='`';
+//     ss[i+1]='`';
+//     int j=i+1;
+//     for (; s[i]; i++)ss[++j]=s[i];
+//     for (int t = 0; ss[t]; t++)printf("%c ",ss[t]);
+//     return 0;
+// }
+
+#include<stdio.h>
 int main()
 {
-    scanf("%s",s);
-    int i=0;
-    while (s[i]||s[i]!='"')i++;
-    if(s[i]=='\n')return 0;
-    memcpy(ss,s,sizeof(char)*i);
-    ss[i]='`';
-    ss[i+1]='`';
-    int j=i+1;
-    for (; s[i]; i++)ss[++j]=s[i];
-    for (int t = 0; ss[t]; t++)printf("%c ",ss[t]);
+    int c, q = 1;
+    while((c = getchar()) != EOF)
+    {
+        if(c == '"')
+        {
+            printf("%s", q ? "``" : "''");
+                q = !q;
+        }
+        else printf("%c", c);
+    }
     return 0;
 }
