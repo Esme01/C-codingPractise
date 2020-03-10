045a0eee60f74abd1d4e6f6d9aad445dda38aa02
Esme01 <TXQ01020304@163.com>
2020-03-10 23:32:06 +0800

    1

--- build/test.cpp
+++ build/test.cpp
@@ -1,22 +1,55 @@
 #include<iostream>
+#include<stdio.h>
+#include<string.h>
+
 using namespace std;
-#define mod 987654321
-typedef long long ll;
-ll cal1(ll n){
-	ll x=1;
-	for(ll i=1;i<=n;i++){
-		x=x*i%mod;
-	}
-	return x;
-}
-ll cal2(ll n,ll m){
-	return cal1(n)/(cal1(m)*cal1(n-m)%mod);
-}
-int main(){
-	ll n,m,s=0;
-	cin>>n>>m;
-	for(ll i=1;i<=n;i++)
-	s=s+cal2(n,m)%mod*cal2(n,i)%mod*i%mod*i%mod*i%mod;
-	cout<<s<<endl;
-	return 0;
+const int maxn=1000;
+struct bign
+{
+    int len,s[maxn];
+    bign(){memset(s,0,sizeof(s));len=1;}
+    /**
+     * 构造体中有一个函数，成为构造函数（constructor）
+     * 是C++中特有的，作用是初始化
+    */
+    bign operator=(const char* num)
+    {
+        len = strlen(num);
+        for (int i = 0; i < len; i++)s[i]=num[len-i-1]-'0';
+        return *this;
+    }
+    /**
+     * 可以把这个字符串转化为“逆序数组+长度”的内部表示方法
+    */
+   bign operator = (int num)
+   {
+       char s[maxn];
+       sprintf(s,"%d",num);
+       *this=s;
+       return *this;
+   }
+   /**
+    * 支持x=1234这样更常用的赋值方式
+   */
+
+    bign(int num){*this=num;}
+    bign(const char* num){*this=num;}
+    //让代码支持“初始化”操作
+
+    /**
+     * 以上只是简单地调用刚才的赋值运算符，接下来需要提供一个函数把它转换为字符串
+     *
+    */
+    string str()const
+    {
+        string res="";
+        for (int i = 0; i < len; i++)res=(char)(s[i]+'0')+res;//字符串的连接？
+        if(res=="")res="0";
+        return res;
+    }
+};
+
+int main()
+{
+
 }
