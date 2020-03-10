d2c41bee7e23ddf22cb59ca9c735a510ceac3046
Esme01 <TXQ01020304@163.com>
2020-03-10 18:11:54 +0800

    factorial

---
+++ build/Ⅴalgorithm/ExactValueOfFactorial.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stdio.h>
+#define MAXN 3000
+int f[MAXN]={0};
+
+using namespace std;
+int main()
+{
+    int a,i,b,n,temp=0,s=MAXN;
+    cin>>n;
+    f[0]=1;
+    for (i = 2; i <= n; i++)
+    {
+        for (int j = 0; j < 3000; j++)
+        {
+            f[j]=(temp+f[j]*i)%10;
+            temp=(temp+f[j]*i)/10;
+        }
+    }
+    while (f[s]==0)s++;
+    for ( ; s>=0 ; s--)
+    {
+        cout<<f[s];
+    }
+    cout<<endl;
+    return 0;
+}
