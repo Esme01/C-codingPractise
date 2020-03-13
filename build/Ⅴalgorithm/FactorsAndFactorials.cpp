d38afb901764ef05cacdda25e37da444a98d6591
Esme01 <TXQ01020304@163.com>
2020-03-13 23:18:43 +0800

    factor

---
+++ build/Ⅴalgorithm/FactorsAndFactorials.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    int p[100]={0},flag,t=0;
+    for (int i = 2; i <= 100; i++)
+    {
+        flag=1;
+        for (int j = 2; j <= sqrt(i); j++)
+        //必须是j <= sqrt(i)，不能少了=，不然4也是“素数”
+        {
+            if (i%j==0)
+            {
+                flag=0;
+                break;
+            }
+        }
+        if (flag==1)p[t++]=i;
+    }
+    
+
+    int n,k=0;
+    long long m=1;
+    int s[30]={0};
+    cin>>n;
+
+    for (int j = 1; j <= n; j++)m*=j;//53！就超了
+    for (;;)
+    {
+        if (p[k]==0)break;
+        while (m%p[k]==0)
+        {
+            s[k]++;
+            m/=p[k];
+        }
+        if(m==1)break;
+        k++;
+    }
+    
+    for (int i = 0; i < 30; i++)
+            printf("%d ",s[i]);
+    return 0;
+
+}
--- build/Ⅴalgorithm/cantor.cpp
+++ build/Ⅴalgorithm/cantor.cpp
@@ -2,27 +2,42 @@
  #include<stdio.h>
  #include<stdlib.h>
  #include<algorithm>
+ #include<math.h>
  #include<string.h>
 
 using namespace std;
 
+// int main()
+// {
+//     int n;
+//     while (scanf("%d",&n)==1)
+//     {
+//         int k=1,s=0;
+//         for(;;)
+//         {
+//             s+=k;
+//             if (s>=n)//所求项是第K条对角线的倒数第s-n+1个元素
+//             {
+//                 printf("%d/%d",s-n+1,k-s+n);
+//                 break;
+//             }
+//             k++;
+            
+//         }
+//     }
+//     return 0;
+// }
+
+//代数的解法
 int main()
 {
     int n;
     while (scanf("%d",&n)==1)
     {
-        int k=1,s=0;
-        for(;;)
-        {
-            s+=k;
-            if (s>=n)//所求项是第K条对角线的倒数第s-n+1个元素
-            {
-                printf("%d/%d",s-n+1,k-s+n);
-                break;
-            }
-            k++;
-            
-        }
+        int k=(int)floor((sqrt(8.0*n+1)-1)/2-1e-9)+1;
+        int s=k*(k+1)/2;
+        printf("%d/%d",s-n+1,k-s+n);
     }
     return 0;
+    
 }
