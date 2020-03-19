cb440a5a967d915240d8e5df28f11285c2a6b7ba
Esme01 <TXQ01020304@163.com>
2020-03-19 15:25:02 +0800

    1

--- build/Ⅴalgorithm/TreesInTheOrchard.cpp
+++ build/Ⅴalgorithm/TreesInTheOrchard.cpp
@@ -1,11 +1,12 @@
 #include <cstdio>
 double area(double x0, double y0, double x1, double y1, double x2, double y2){
-	double a = x0 * y1 + x1 * y2 + x2 * y0 - x0 * y2 - x1 * y0 - x2 * y1;
+	double a = x0 * y1 + x1 * y2 + x2 * y0 - x0 * y2 - x1 * y0 - x2 * y1;//三角形有向面积的两倍
 	if (a < 0.0){
 		a = -a;
 	}
 	return a;
 }
+
 int main(){
 	double x0 = 0, y0 = 0, x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 	while (scanf("%lf%lf%lf%lf%lf%lf", &x0, &y0, &x1, &y1, &x2, &y2) == 6){
@@ -17,7 +18,7 @@ int main(){
 				double a2 = area(x0, y0, x1, y1, x, y)
 					+ area(x0, y0, x, y, x2, y2)
 					+ area(x, y, x1, y1, x2, y2);
-				if (a > a2 - 1e-9 && a < a2 + 1e-9){
+				if (a > a2 - 1e-9 && a < a2 + 1e-9){//存在误差范围
 					++count;
 				}
 			}
