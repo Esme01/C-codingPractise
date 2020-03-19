a509473d49e262b481e4e8772b7b3d64de2fe819
Esme01 <TXQ01020304@163.com>
2020-03-19 11:58:26 +0800

    practise

--- build/test.cpp
+++ build/test.cpp
@@ -1,28 +1,51 @@
- #include<iostream>
- #include<stdio.h>
- #include<stdlib.h>
- #include<algorithm>
- #include<string.h>
+#include<string.h>
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
 
 using namespace std;
+bool is_prime(int n)
+{
+    if (n==2)return true;
+    if(n<2)return false;
+    for (int i = 2; i < sqrt(n); i++)
+        if(n%i==0)return false;
+    return true;
+}
 
+int prime[100],count=0;
 int main()
 {
-    int n;
+    int n,p[100];
+    cin>>n;
+    //求100以内的素数表
+    for (int i = 2; i <=100; i++)
+        if(is_prime(i))prime[count++]=i;
     while (scanf("%d",&n)==1)
     {
-        int k=1,s=0;
-        for(;;)
+        printf("%d!=",n);
+        memset(p,0,sizeof(p));
+        int maxp=0;
+        for (int i = 1; i <=n; i++)
         {
-            s+=k;
-            if (s>=n)//所求项是第K条对角线的倒数第s-n+1个元素
+            int m=i;
+            for (int j = 0; j < count; j++)
             {
-                printf("%d/%d",s-n+1,k-s+n);
-                break;
+                while (m%prime[j]==0)
+                {
+                    m/=prime[j];
+                    p[j]++;
+                    if(j>maxp)maxp=j;
+                }
             }
-            k++;
+            
             
         }
+        for(int i=0;i<=maxp;i++)
+                printf("%d",p[i]);
+            printf("\n");
+        
     }
     return 0;
+
 }
