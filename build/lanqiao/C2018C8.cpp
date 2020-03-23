2dd5f3c31053838befac2b0abe0d70c225eba044
Esme01 <TXQ01020304@163.com>
2020-03-23 09:17:19 +0800

    triangle

--- build/lanqiao/C2018C8.cpp
+++ build/lanqiao/C2018C8.cpp
@@ -5,20 +5,34 @@
 #include<iostream>
 #include<stdio.h>
 #include<string.h>
+#include<sstream>
 
 using namespace std;
 int main()
 {
-    char s[300];
-    int n,m=1,j=0;
-    cin>>n;
-    while (strlen(s)<2*(2*n-2))//尽管输入，之后再删除
-    {
-        sscanf(s,"%d",m);//会自动进行字符串的累加吗
-        m++;
-    }
-    cout<<s<<endl;
-    return 0;
-    
-    
+   stringstream ss;
+   int n,t=1;
+   cin>>n;
+   while (ss.str().size()<4*n-4)
+   {
+       ss<<t;
+       t++;
+   }
+   string s=ss.str();
+   for (int i = 0; i < n-1; i++)cout<<".";
+   cout<<s[0]<<endl;
+   //key:如何输出？
+   for (int j = 2; j < n; j++)//负责输出到n-1行
+   {
+        for(int e=1;e<=n-j;e++)cout<<'.';
+        cout<<s[j-1];
+        //输出中间空格
+        for(int a=1;a<=2*j-3;a++)cout<<'.';
+        cout<<s[4*n-4-1-j-2]<<endl;
+   }
+   
+   //输出最后一行,共2*n-1个数字
+   for (int i = n-1; i < n-1+2*n-1; i++)cout<<s[i];
+   cout<<endl;
+   return 0;
 }
