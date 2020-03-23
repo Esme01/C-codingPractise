7ba28eefe5b7dca2743baa3f93c334b9520b47f3
Esme01 <TXQ01020304@163.com>
2020-03-23 23:34:21 +0800

    1

--- build/test.cpp
+++ build/test.cpp
@@ -5,14 +5,33 @@
 
 using namespace std;
 
+int global_id=0
+class Student
+{
+public:
+   Student(const string &name,int age,bool male,double high,double weight):name(name),age(age),id(id),male(male),high(high),weight(weight){
+      this->id=++global_id;//简洁的写法
+      if (age>0)
+      {
+         this->age=age;
+      }
+   };
+   //this->name=name
+
+
+private:
+   string name;
+   int age;
+   int id;
+   bool male;
+   double high;
+   double weight;
+};
+
+
+
 int main()
 {
-   stringstream ss;
-   string s="1234";
-   int x;
-   ss.clear();
-   ss<<s;
-   ss>>x;
-   cout<<x<<endl;
    
+   return 0;
 }
