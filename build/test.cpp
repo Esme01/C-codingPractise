87f8754ffe94685d4c4893d6e2edb0c185235849
Esme01 <TXQ01020304@163.com>
2020-03-31 13:53:05 +0800

    1

--- build/test.cpp
+++ build/test.cpp
@@ -1,29 +1,18 @@
-#include <iostream>
-#include <set>
-using namespace std;
-set<int>s;
-void setprint(int cnt){
-    cout << "Test output :" << cnt << ":" << endl;
-    for(set<int>::iterator it = s.begin(); it!= s.end(); it++)
-        cout << *it << " ";
-    puts("");
-    return ;
-}
-int main(){
-    int cnt = 1;
-    s.insert(1);
-    s.insert(2);
-    s.insert(5);
-    setprint(cnt++);
- 
-    s.insert(2); 
-//set只允许用一个值出现一次，要插入相同元素请用multiset
-    setprint(cnt++);
- 
-    int a[4] = {11,12,13,14};
-    s.insert(a,a+4); 
-//将区间[a, a+4]里的元素插入容器
-    setprint(cnt++);
- 
-    return 0;
+#include<cstdio>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,p[10];
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)cin>>p[i];
+    sort(p,p+n);
+    do
+    {
+        for(int i=0;i<n;i++)cout<<p[i]<<" ";
+        cout<<endl;
+    } while (next_permutation(p,p+n));
+    return 0;
 }
