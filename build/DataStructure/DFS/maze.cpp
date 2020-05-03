f31785011ad35ab3d79a6faecd1a52a34264022d
Esme01 <TXQ01020304@163.com>
2020-05-03 08:56:49 +0800

    1

---
+++ build/DataStructure/DFS/maze.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+//栈中元素是一个由行、列、方向组成的三元组
+typedef struct 
+{
+    int x,y;//当前访问的迷宫格子的纵横坐标
+    int di;//当前方向
+}Box;
+
+bool findPath(int maze[M+2][N+2],Direction direct[],Stack &s)
+{
+    Box temp;
+    int x,y,di;
+    
+}
+
+int main()
+{
+    return 0;
+}
