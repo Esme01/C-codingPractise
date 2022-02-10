900324fcd549dcbc94f2d2786db049bf6737376f
Esme01 <TXQ01020304@163.com>
2022-02-10 15:48:30 +0800

    2

---
+++ build/string/twopionts.cpp/unrepeat.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+const int N=100010;
+int main()
+{
+/*
+
+    int a[N];
+    int n,res=0;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i = 0,j=0; i < n; i++)
+    {
+        j=i+1;
+        while (a[j]!=a[i]&&j<n)j++;
+        if(res<j-i)res=j-i;
+    }
+    cout<<res<<endl;
+*/
+
+
+
+}
